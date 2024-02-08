69db38c92e6b0b36ddd6f713da0f270e4a7a51c2
Justin <ju3tin@hotmail.co.uk>
2024-02-08 04:35:02 +0000

    Update index.js

--- index.js
+++ index.js
@@ -62,6 +62,35 @@ app.get('/dude.json', (req, res) => {
 
 })
 
+app.post('/upload-avatar', async (req, res) => {
+    try {
+        if(!req.files) {
+            res.send({
+                status: false,
+                message: 'No file uploaded'
+            });
+        } else {
+            //Use the name of the input field (i.e. "avatar") to retrieve the uploaded file
+            let avatar = req.files.avatar;
+            
+            //Use the mv() method to place the file in the upload directory (i.e. "uploads")
+            avatar.mv('./upload/' + avatar.name);
+
+            //send response
+            res.send({
+                status: true,
+                message: 'File is uploaded',
+                data: {
+                    name: avatar.name,
+                    mimetype: avatar.mimetype,
+                    size: avatar.size
+                }
+            });
+        }
+    } catch (err) {
+        res.status(500).send(err);
+    }
+});
 
 app.post("/upload", upload.single('productImage'), (req, res, next) => {
   
